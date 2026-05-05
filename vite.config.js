75e96e25b6a0c01c1a14b54db69aeee7273b3b83
Olushola Ogunkelu <sogunkelu@gmail.com>
2026-05-05 22:16:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'home.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        programs: resolve(__dirname, 'programs.html'),
+      }
+    }
+  },
   server: {
     open: true,
     watch: {
@@ -9,5 +21,3 @@ export default defineConfig({
     }
   }
 })
-
- 
